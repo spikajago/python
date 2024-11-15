e8f43c651715a2e9329c13f8fa597ef022fff0bd
spikajago <steven@hkhk.edu.ee>
2024-11-15 14:00:45 +0200

    Add files via upload

---
+++ yl13.py
@@ -0,0 +1,36 @@
+# 15.11.24 Steven Pikajago
+# ülesanded 13
+
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+# Küsi kasutajalt numbrilist sisendit
+#nr = screen.numinput("Vanuse sisestamine", "Mis on sinu vanus?", default=20, minval=0, maxval=100)
+nr=10
+
+for i in range(nr*10):
+    t.lt(90)
+    t.fd(3)
+    t.lt(180)
+    t.fd(3)
+    t.lt(90)
+    t.fd(4)
+t.goto(0,0)
+
+for i in range(nr+1):
+    t.lt(90)
+    t.fd(5)
+    t.write(i, font=("Arial", 10, "normal"))
+    t.lt(180)
+    t.fd(5)
+    t.lt(90)
+    t.fd(10*4)
+
+
+
+
+
+
+
+
+turtle.done()
