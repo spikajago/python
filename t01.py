097828df417f748f6a5e6c8889a64c4348e4e43c
spikajago <steven@hkhk.edu.ee>
2025-01-27 12:51:11 +0200

    Add files via upload

--- t01.py
+++ t01.py
@@ -4,15 +4,10 @@ import requests
 url = "https://dummy-json.mock.beeceptor.com/roles"
 
 response = requests.get(url)
-
+otsing=input("Otsi rolle ")
 if response.status_code == 200:
     data=response.json()
     for valik in data:
-         nimi=valik['name']
-         print(nimi)
-
-
-
-
+        nimi=valik['name']
     print("Viga andmete allalaadimisel:", response.status_code)
           
